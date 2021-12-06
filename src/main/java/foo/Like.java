db8f90e49a6af5630bd74fd50223c5115771bc57
Amar Tious <tiousamar@gmail.com>
2021-12-06 13:24:15 +0000

    corrected a typo

--- src/main/java/foo/Like.java
+++ src/main/java/foo/Like.java
@@ -4,7 +4,7 @@ public class Like {
 	public String postId;
     public String userId;
 	
-	public PostMessage(String post, String user) {
+	public Like(String post, String user) {
         this.postId = post;
         this.userId = user; 
     }
